0b0060cec08873053c9f5cddd55c3866ae7e85b5
Sugatraj <sugatraj.2106@gmail.com>
2025-06-09 02:48:37 +0530

    enhance OutletNotFound component with improved layout and visual elements; add image and styled message to provide clearer guidance for users encountering outlet issues

--- src/components/OutletNotFound.jsx
+++ src/components/OutletNotFound.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function OutletNotFound() {
   return (
-    <div>Outlet having issue. Rescan the QR Code again!</div>
+    <div className="container min-vh-100 d-flex align-items-center justify-content-center">
+      <div className="text-center p-4 rounded-4">
+        <img 
+          src="/src/assets/images/scanQr.gif" 
+          alt="Scan QR Code" 
+          className="img-fluid mb-4 rounded-4"
+          style={{ maxWidth: '200px', height: '200px' }}
+        />
+        <h5 className="mt-3">Outlet having issue. 
+        <br />
+        Rescan the QR Code again!</h5>
+      </div>
+    </div>
   )
 }
 
