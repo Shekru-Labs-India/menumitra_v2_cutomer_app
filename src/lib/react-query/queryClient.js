275cf6f435ab9b7522dbcb5322994c39ae73c8e9
Sugatraj <sugatraj.2106@gmail.com>
2025-07-12 15:16:19 +0530

    Update service worker revision for index.html and increase queryClient staleTime to 60 seconds for improved caching behavior.

--- src/lib/react-query/queryClient.js
+++ src/lib/react-query/queryClient.js
@@ -5,7 +5,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       // Match your current CacheDataContext expiration time
-      staleTime: 30 * 1000, // 30 seconds
+      staleTime: 60 * 1000, // 30 seconds
       cacheTime: 10 * 60 * 1000, // 10 minutes
       refetchOnWindowFocus: false,
       retry: 1,
