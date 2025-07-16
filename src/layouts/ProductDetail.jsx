1eeea0573c3f4602457be40027f8c0f18a6beb7f
Avanti2305 <avantigurav.2305@gmail.com>
2025-07-16 16:02:23 +0530

    product Details

--- src/layouts/ProductDetail.jsx
+++ src/layouts/ProductDetail.jsx
@@ -300,14 +300,15 @@ function ProductDetail() {
                   <span className="text-style text-soft">Price</span>
                   <div className="d-flex justify-content-between align-items-center">
                     <h3 className="sub-title mb-0">
-                      ₹{menuDetails.portions[0]?.price}
+                      ₹
+                      {menuDetails.offer > 0
+                        ? menuDetails.portions[0]?.price -
+                          (menuDetails.portions[0]?.price * menuDetails.offer) /
+                            100
+                        : menuDetails.portions[0]?.price}
                       {menuDetails.offer > 0 && (
                         <del className="ms-2 text-muted">
-                          ₹
-                          {Math.round(
-                            menuDetails.portions[0]?.price /
-                              (1 - menuDetails.offer / 100)
-                          )}
+                          ₹{menuDetails.portions[0]?.price}
                         </del>
                       )}
                     </h3>
