a36152ddbfb421b5e70d86b775fd73ea6442e4ad
Sugatraj <sugatraj.2106@gmail.com>
2025-09-24 12:06:03 +0530

    Update service worker revision and modify HorizontalMenuCard styles for improved responsiveness and layout flexibility.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "b078daa49e7326457d1803f1c16acef9"
   }, {
     "url": "index.html",
-    "revision": "0.1bhs37n5oqo"
+    "revision": "0.otkkftdpt3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
